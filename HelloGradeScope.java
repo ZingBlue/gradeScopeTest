c2570ccaae457da55bb66bfdf79a4517a71b1caf
Hayden <23709367+ZingBlue@users.noreply.github.com>
2023-06-28 17:29:42 -0700

    Upload GradeScope Java

---
+++ HelloGradeScope.java
@@ -0,0 +1,16 @@
+/**
+ * THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING A TUTOR OR CODE WRITTEN BY OTHER STUDENTS - Hayden
+*/
+// Note: for this assignment most of the code was already written (as permitted), so I clearly did not write those portions.
+
+public class HelloGradeScope{
+	
+	public HelloGradeScope(){
+	}
+	
+	public String sayHi(){
+		return "Hello GradeScope";
+	}
+	
+}
+// Overall, the setup process of GradeScope was pretty simple, and I have used Github in the past so setting up a repo wasn't to bad either :)
